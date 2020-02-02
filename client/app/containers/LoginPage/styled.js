1ac7006e8f1c51b5e9d055c41490f2178283eb88
Sepehr Hosseini <iamsepehrhosseini@gmail.com>
2020-02-02 14:02:15 +0400

    feat(client/auth): add auth pages

---
+++ client/app/containers/LoginPage/styled.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import CardUI from '@material-ui/core/Card';
+import TextFieldUI from '@material-ui/core/TextField';
+
+export const Card = styled(CardUI)`
+  max-width: 360px;
+  min-width: 300px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100vh;
+
+  @media (min-width: 960px) {
+    padding: 10px;
+  }
+`;
+
+export const TextField = styled(TextFieldUI)`
+  width: 100%;
+`;
+
+export const FormRow = styled.div`
+  padding: 15px;
+`;
+
+export const CardHeader = styled.div`
+  padding: 15px;
+  padding-bottom: 0;
+`;
---
+++ client/app/containers/RegisterPage/styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import CardUI from '@material-ui/core/Card';
+import TextFieldUI from '@material-ui/core/TextField';
+
+export const Card = styled(CardUI)`
+  max-width: 360px;
+  min-width: 300px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100vh;
+
+  @media (min-width: 960px) {
+    padding: 10px;
+  }
+`;
+
+export const TextField = styled(TextFieldUI)`
+  width: 100%;
+`;
+
+export const FormRow = styled.div`
+  padding: 15px;
+`;
